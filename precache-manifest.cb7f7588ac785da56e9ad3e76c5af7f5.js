58af603d1fb40ed2f7999a0cfa01e8901441e3bc
codepujan <paudelpujan786@gmail.come>
2021-11-10 02:06:29 -0500

    Updates

---
+++ precache-manifest.cb7f7588ac785da56e9ad3e76c5af7f5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7981e5194c97c2eb7a4f0dc05616a6ec",
+    "url": "/pujanpaudel/index.html"
+  },
+  {
+    "revision": "d1b3f863743a0a2776b7",
+    "url": "/pujanpaudel/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "69b133449be594091dcb",
+    "url": "/pujanpaudel/static/css/main.e1157af2.chunk.css"
+  },
+  {
+    "revision": "d1b3f863743a0a2776b7",
+    "url": "/pujanpaudel/static/js/2.1425f718.chunk.js"
+  },
+  {
+    "revision": "69b133449be594091dcb",
+    "url": "/pujanpaudel/static/js/main.5f09cc3c.chunk.js"
+  },
+  {
+    "revision": "a646151d538905f71028",
+    "url": "/pujanpaudel/static/js/runtime-main.88be7a51.js"
+  },
+  {
+    "revision": "ed4fb1da5c28c8ae465999e7adbc5ef2",
+    "url": "/pujanpaudel/static/media/main.ed4fb1da.jpeg"
+  },
+  {
+    "revision": "9014bd9cfeeba952d517d4e8cff3b2dc",
+    "url": "/pujanpaudel/static/media/paper1.9014bd9c.png"
+  },
+  {
+    "revision": "994bfb08bdeab1e78064760e83847d73",
+    "url": "/pujanpaudel/static/media/picture.994bfb08.jpeg"
+  }
+]);
